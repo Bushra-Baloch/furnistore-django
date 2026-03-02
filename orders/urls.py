0ee42fad39157e72f0c52f23780fb5f15d21f1a1
Bushra Baloch <bushrabaloch2020@gmail.com>
2026-03-02 22:55:49 +0500

    Upgraded Orders urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.order_list, name='order_list'),
-    path('<int:order_id>/', views.order_detail, name='order_detail'),
-    path('success/', views.order_success, name='order_success'),
+urlpatterns = [ 
+    # Order List
+    path('', views.order_list, name='list'),
+
+    # Order Detail
+    path('<int:order_id>/', views.order_detail, name='detail'),
+
+    # Order Success
+    path('success/', views.order_success, name='success'),
 ]
