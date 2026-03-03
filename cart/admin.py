e5737afd984cbd66359a1ccb953c570c639f4b99
Bushra Baloch <bushrabaloch2020@gmail.com>
2026-03-03 23:19:32 +0500

    Added admin configuration for Cart and CartItem models

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+    readonly_fields = ('product', 'quantity', 'added_at')
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'updated_at')
+    search_fields = ('user__username', 'user__email')
+    list_filter = ('created_at', 'updated_at')
+    inlines = [CartItemInline]
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity', 'added_at')
+    search_fields = ('product__name',)
