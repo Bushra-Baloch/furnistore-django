7721c34fde34e54ab1e278dc0133db60f8998b67
Bushra Baloch <bushrabaloch2020@gmail.com>
2026-03-04 22:28:51 +0500

    Added Profile  admin configuration for user accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'user',
+        'phone',
+        'city',
+        'created_at'
+    )
+
+    search_fields = (
+        'user__username',
+        'phone'
+    )
+
+    list_filter = (
+        'city',
+    )
