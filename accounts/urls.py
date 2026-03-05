66ce599d21086b9f696753dd98b08d4e9903b253
Bushra Baloch <bushrabaloch2020@gmail.com>
2026-03-05 16:47:12 +0500

    Added password change functionality using Django auth views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -7,4 +8,21 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path("profile/", views.profile_view, name="profile"),
     path("profile/edit/", views.edit_profile, name="edit_profile"),
+     # Password change
+    path(
+        "change-password/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="accounts/change_password.html"
+        ),
+        name="change_password",
+    ),
+
+    path(
+        "change-password/done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="accounts/change_password_done.html"
+        ),
+        name="password_change_done",
+    ),
+
 ]
